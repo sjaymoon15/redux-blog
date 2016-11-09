389ecb3d8e4c6e3888e13fb9917e537fed2cfdc5
Seungjae Moon <sjaymoon15@gmail.com>
2016-11-09 15:47:06 -0500

    remove greeting route

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostIndex from './components/posts_index'
 
-const Greeting = () => {
-	return <div>MMM</div>;
-}
 
 export default (
 	<Route path="/" component={App}>
-		<Route path="greet" component={Greeting} />
-		<Route path="greet2" component={Greeting} />
-		<Route path="greet3" component={Greeting} />
+		<IndexRoute component={PostIndex} />
 	</Route>
 
 );
