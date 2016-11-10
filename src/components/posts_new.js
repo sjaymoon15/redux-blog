aeb90e98e79c906641d98b91d3265587962762ce
Seungjae Moon <sjaymoon15@gmail.com>
2016-11-10 15:54:25 -0500

    add redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
 	render() {
+		// const handleSubmit = this.props.handleSubmit;
+		// const title = this.props.fields.title; ...
+		const { fields: {title, categories, content}, handleSubmit } = this.props;
+		console.log(title);
 		return (
-			<div>create form</div>
+			<form onSubmit={handleSubmit(this.props.createPost)} >
+				<h3>Create A New Post </h3>
+				<div className='form-group'>
+					<label>Title</label>
+					<input type='text' className='form-control' {...title}/>
+				</div>
+				<div className='form-group'>
+					<label>Categories</label>
+					<input type='text' className='form-control' {...categories}/>
+				</div>
+				<div className='form-group'>
+					<label>Content</label>
+					<textarea className='form-control' {...content}/>
+				</div>
+				<button type='submit' className='btn btn-primary'>Submit</button>
+			</form>
 		)
 	}
 }
 
-export default PostsNew;
+//connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+//reduxForm 1st is form config, 2nd mapStateToProps, 3rd mapDispatchToProps
+
+export default reduxForm({
+	form: 'PostsNewForm',
+	fields: ['title', 'categories', 'content']
+}, null, { createPost } )(PostsNew);
+// user types something in.. record it on app level state
+// state=== {
+// 	form: {
+// 		PostsNewForm : {
+// 			title: '...',
+// 			categories: '...',
+// 			content: '...'
+// 		}
+// 	}
+// }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import PostReducer from './reducer_posts';
+import { reducer as formReducer } from 'redux-form';
 
+
+//has to be 'form'
 const rootReducer = combineReducers({
-	posts: PostReducer	
+	posts: PostReducer,
+	form: formReducer
 });
 
 export default rootReducer;
